2cdf166f2a8c826877645d6861b2d1fc8e1276c4
mydatahack <taka.honda@hotmail.com>
2019-11-11 22:35:25 +1100

    updated service worker & mobile zoom css property

--- mtd/sw.js
+++ mtd/sw.js
@@ -1,11 +1,17 @@
+var staticCacheName = 'mtdApp-cache-v1'
 
 self.addEventListener('install', function(e) {
   e.waitUntil(
-    caches.open('mtdApp').then(function(cache) {
+    caches.open(staticCacheName).then(function(cache) {
+      // return cache.addAll([
+      //   '/mtd/index.html',
+      //   '/mtd/default.js',
+      //   '/mtd/style/app.css'
+      // ]);
       return cache.addAll([
-        '/mtd/index.html',
-        '/mtd/default.js',
-        '/mtd/style/app.css'
+        '/index.html',
+        '/default.js',
+        '/style/app.css'
       ]);
     })
   );
@@ -18,3 +24,22 @@ self.addEventListener('install', function(e) {
     })
   );
  });
+
+ self.addEventListener('activate', event => {
+  console.log('Activating new service worker...');
+
+  const cacheWhitelist = [staticCacheName];
+
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            console.log('deleting old cache: ', cacheName)
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
