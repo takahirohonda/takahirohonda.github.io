f74a3db94811bc2df5735cdeea9815c010ef58b2
mydatahack <taka.honda@hotmail.com>
2019-11-12 21:37:26 +1100

    updated sw.js version upgrade plus scroll css on body -mtd

--- mtd/sw.js
+++ mtd/sw.js
@@ -1,5 +1,4 @@
-var staticCacheName = 'mtdApp-cache-v3'
-
+var staticCacheName = 'mtdApp-cache-v1573554716143'
 self.addEventListener('install', function(e) {
   e.waitUntil(
     caches.open(staticCacheName).then(function(cache) {
