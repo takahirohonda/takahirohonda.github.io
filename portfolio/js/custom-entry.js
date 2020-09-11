b1bcb2529dbc5079a5e70c050951dfe0d9a3d42a
mydatahack <43569574+mydatahack@users.noreply.github.com>
2020-09-11 05:03:57 +0000

    Deploying to master from  @ 77e0981dd2e51087a328e6d8cddcc3dc649c502f 🚀

--- portfolio/js/custom-entry.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('dom loaded event caught')
-  th.public.initScrollEventListener();
-  th.public.initNavBurgerClickEventListner();
-})
