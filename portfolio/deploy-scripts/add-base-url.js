8808c12cec4dfc6e1e7a4abd4ec083fd92b7b130
mydatahack <43569574+mydatahack@users.noreply.github.com>
2020-09-11 07:06:42 +0000

    Deploying to master from  @ b108858225839270b5b506c9cbf3f209f04c8982 🚀

--- portfolio/deploy-scripts/add-base-url.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-
-const line = 'baseUrl: https://mydatahack.github.io/portfolio/';
-fs.writeFileSync(path.join(__dirname, '../_data/baseUrl.yml'), line);
-
-console.log(`Updated baseUrl successfully`);
-console.log(line);
