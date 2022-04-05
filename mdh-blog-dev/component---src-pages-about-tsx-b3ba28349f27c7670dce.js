b836c9258ec6606ac4a8a1d705ac4729bc4665b4
mydatahack <43569574+mydatahack@users.noreply.github.com>
2022-04-05 07:08:14 +0000

    Deploying to master from  @ 41eec10e9d2f22955c84fa4e880e411d4a55ecc9 🚀

---
+++ mdh-blog-dev/component---src-pages-about-tsx-b3ba28349f27c7670dce.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmydatahack_blog_site_gatsby=self.webpackChunkmydatahack_blog_site_gatsby||[]).push([[49],{8509:function(e,n,a){a.r(n);var t=a(4917),i=a(5893);n.default=function(){return(0,i.jsxs)(i.Fragment,{children:[(0,i.jsx)(t.nz,{}),(0,i.jsx)("main",{children:(0,i.jsx)(t.OO,{children:(0,i.jsx)(t.eT,{children:"Building AlexNet with TensorFlow and Running it with AWS SageMaker"})})})]})}}}]);
+//# sourceMappingURL=component---src-pages-about-tsx-b3ba28349f27c7670dce.js.map
---
+++ mdh-blog-dev/component---src-pages-heading-tsx-bde84f85f5c883ab40dd.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmydatahack_blog_site_gatsby=self.webpackChunkmydatahack_blog_site_gatsby||[]).push([[570],{6520:function(n,e,i){i.r(e);var s=i(4917),t=i(5893);e.default=function(){return(0,t.jsxs)(t.Fragment,{children:[(0,t.jsx)(s.nz,{}),(0,t.jsx)("main",{children:(0,t.jsxs)(s.OO,{children:[(0,t.jsx)(s.eT,{children:"Building AlexNet with TensorFlow and Running it with AWS SageMaker"}),(0,t.jsx)(s.S4,{children:"This is not working..."})]})})]})}}}]);
+//# sourceMappingURL=component---src-pages-heading-tsx-bde84f85f5c883ab40dd.js.map
