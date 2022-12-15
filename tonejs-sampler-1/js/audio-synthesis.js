d796099c48051bdcddf187c41faf1c68ff07c830
mydatahack <43569574+mydatahack@users.noreply.github.com>
2022-12-15 01:34:00 +0000

    Deploying to master from  @ 5e440494f03e0947d5b4ddcd6d83d7b60eedbef1 🚀

--- tonejs-sampler-1/js/audio-synthesis.js
+++ tonejs-sampler-1/js/audio-synthesis.js
@@ -2,29 +2,28 @@ Tone.Transport.bpm.value = 107;
 
 // Setting Gain
 const gainKick = new Tone.Gain(1);
-const gainHiHat = new Tone.Gain(0.1);
+const gainHiHat = new Tone.Gain(0.8);
 const gainSnare = new Tone.Gain(2);
 const gainBass = new Tone.Gain(2.5);
 const gainPolySynth = new Tone.Gain(0.6);
 const gainLeadSynth = new Tone.Gain(0.2);
 
-// Effect
+// Effects
 const distortion = new Tone.Distortion({
   distortion: 0.1,
   oversample: '4x', // none, 2x, 4x
 });
 const reverb = new Tone.Reverb(0.75, 1000);
 
-// Setup a reverb with ToneJS
 const reverbLeadSynth = new Tone.Reverb({
   decay: 4,
   wet: 0.2,
   preDelay: 0.25,
 });
 
-
-
 const feedbackDelay = new Tone.FeedbackDelay('8n', 0.25);
+
+// Compressor
 const compressor = new Tone.Compressor({
   ratio: 5,
   threshold: -12,
@@ -33,7 +32,22 @@ const compressor = new Tone.Compressor({
   knee: 10,
 });
 
-gainKick.chain(compressor, Tone.Master);
+// Equalizer - 3 band frequencies (https://tonejs.github.io/docs/14.7.77/EQ3)
+const hiHatEq = new Tone.EQ3(
+  {
+    lowLevel: 0.5,
+    midLevel: 1,
+    highLevel: 2.5
+  }
+);
+
+// Filter - https://tonejs.github.io/docs/14.7.77/Filter
+const lowPassfilterForKick = new Tone.Filter({
+  frequency: 8000,
+});
+
+// Kick
+gainKick.chain(lowPassfilterForKick, compressor, Tone.Master);
 const kick = new Tone.MembraneSynth({
   pitchDecay:0.05,
   octaves: 4,
@@ -53,13 +67,20 @@ const kick = new Tone.MembraneSynth({
   }
 }).connect(gainKick);
 
-gainHiHat.chain(reverb, Tone.Master);
+const kickNote = 'E1';
+const kickSequence = new Tone.Sequence((time, note) => {
+  kick.triggerAttackRelease(note, 0.1, time);
+}, [kickNote, [ ,kickNote], kickNote, , , kickNote, , kickNote,])
+  .start(0);
+
+// Hi-Hat
+gainHiHat.chain(hiHatEq, reverb, Tone.Master);
 const hiHat = new Tone.MetalSynth({
   frequency  : 600,
   envelope  : {
     attack  : 0.001 ,
-    decay  : 1.4 ,
-    release  : 0.2
+    decay  : 0.03 ,
+    release  : 0.05
   }  ,
   harmonicity  : 5.1 ,
   modulationIndex  : 32 ,
@@ -67,6 +88,11 @@ const hiHat = new Tone.MetalSynth({
   octaves  : 1.5
 }).connect(gainHiHat);
 
+const hiHatLoop = new Tone.Loop(time => {
+  hiHat.triggerAttackRelease('C6', '8n', time);
+}, '8n').start(0);
+
+// Snare
 gainSnare.chain(Tone.Master);
 const snareDrum = new Tone.NoiseSynth({
   volume: 2,
@@ -82,24 +108,6 @@ const snareDrum = new Tone.NoiseSynth({
   },
 }).connect(gainSnare);
 
-// const lowPass = new Tone.Filter({
-//   frequency: 8000,
-// }).toMaster();
-
-const kickLoop = new Tone.Loop(time => {
-  kick.triggerAttackRelease('C1', '8n', time);
-}, '4n');
-const kickNote = 'E1';
-const kickSequence = new Tone.Sequence((time, note) => {
-  kick.triggerAttackRelease(note, 0.1, time);
-}, [kickNote, [ ,kickNote], kickNote, , , kickNote, , kickNote,])
-  .start(0);
-
-const hiHatLoop = new Tone.Loop(time => {
-  hiHat.triggerAttackRelease('C6', '8n', time);
-}, '8n').start(0);
-
-
 const snareDrumLoop = new Tone.Loop(time => {
   snareDrum.triggerAttackRelease('2n', time);
 }, '2n').start('4n');
@@ -114,11 +122,10 @@ const bass = new Tone.Synth({
 // Base sequence
 const baseSequence = new Tone.Sequence((time, note) => {
   bass.triggerAttackRelease(note, '4n', time);
-}, [['F#0', 'F#0'], [ ,'E0'], 'F0', , , 'B0', , 'D1',])
+}, [['F#0', 'F#0'], [ ,'Bb0'], 'F0', , , 'B0', , 'D1',])
   .start(0);
 
-// Keybord 1 Sequence
-
+// Synth Chords
 gainPolySynth.chain(feedbackDelay, Tone.Master);
 const highSynth = new Tone.PolySynth({
   volume: -16,
@@ -139,8 +146,7 @@ const Key2Sequence = new Tone.Sequence((time, note) => {
 }, [['C#5', ], 'F#5',,,,,,,] )
   .start(0);
 
-// Keyboard chorus line
-
+// Synth Hook
 gainLeadSynth.chain(reverbLeadSynth, Tone.Master);
 const synthLead = new Tone.Synth({
   oscillator : {
@@ -161,7 +167,7 @@ const synthLeadSequence3 = new Tone.Sequence((time, note) => {
   synthLead.triggerAttackRelease(note, '16n', time);
 }, ['F#4','F#4','F#4','F#4',,,,,,,,,,,,,])
   .start(0);
-// analyse
 
+// Analyse
 const waveSynth = new Tone.Waveform();
 
