80c77684495081d9c3a98ab422d2fd78b3e247e8
takahirohonda <43569574+takahirohonda@users.noreply.github.com>
2024-08-18 11:14:57 +0000

    Deploying to master from  @ b6de4cc2ceefd57c697841e0c323cb4414471788 🚀

---
+++ learn-german-test1/sw.js
@@ -0,0 +1,44 @@
+var staticCacheName = 'mtdApp-cache-v1573557681712'
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(staticCacheName).then(function(cache) {
+      return cache.addAll([
+        '/mtd/index.html',
+        '/mtd/default.js',
+        '/mtd/style/app.css'
+      ]);
+      // return cache.addAll([
+      //   '/index.html',
+      //   '/default.js',
+      //   '/style/app.css'
+      // ]);
+    })
+  );
+ });
+
+ self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+ });
+
+ self.addEventListener('activate', event => {
+  // console.log('Activating new service worker...');
+
+  const cacheWhitelist = [staticCacheName];
+
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            // console.log('deleting old cache: ', cacheName)
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
