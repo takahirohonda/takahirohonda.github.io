09976936576227bf7b9fff3df3f9c5f77d63c293
takahirohonda <43569574+takahirohonda@users.noreply.github.com>
2024-08-23 08:50:19 +0000

    Deploying to master from  @ 4821851edc009b27b28762e799fa8216835f6c97 🚀

---
+++ learn-german/assets/main-CaQy_8-V.js
@@ -0,0 +1,1 @@
+import{c as e}from"./utils-DwXe3Evf.js";const c=["Wir Suchen Dich.","Döner macht schöner."];(function(){e([...c],"my-favourite-phrases")})();
--- learn-german/assets/main-ZmT1lE48.js
+++
@@ -1,1 +0,0 @@
-import{c as e}from"./utils-CJKMaagY.js";const c=["Wir Suchen Dich.","Döner macht schöner."];(function(){e([...c],"my-favourite-phrases")})();
