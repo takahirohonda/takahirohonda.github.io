25aaa4219fe0f379ef911bfa5a668392bbb1bb41
takahirohonda <43569574+takahirohonda@users.noreply.github.com>
2024-08-27 10:57:55 +0000

    Deploying to master from  @ 92cdc9748dfeaf62a50d9a584df3d462c54c7e42 🚀

---
+++ learn-german/assets/auxiliaryVerbs-BtmVS8hk.js
@@ -0,0 +1,1 @@
+import{c as n}from"./utils-DwXe3Evf.js";const e=["Kann ich das Fenster öffnen?"];(function(){n([...e],"konnen")})();
---
+++ learn-german/assets/questions-CE79GQl-.js
@@ -0,0 +1,1 @@
+import{c as e}from"./utils-DwXe3Evf.js";const n=["Was haben Sie?"];(function(){e([...n],"was")})();
